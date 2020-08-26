80586e34e57e479ed517b9c028e0acdf27d5afe5
kiptoo-097 <kiptoo097@gmail.c0m>
2020-08-26 15:20:17 +0300

    added female and male akan names

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function getAkanName() {
+    event.preventDefault();
+    var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+}
