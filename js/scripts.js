06b4bb1c62643ac3760ba05066c81e8671778199
kiptoo-097 <kiptoo097@gmail.c0m>
2020-08-26 15:22:02 +0300

    added getelement id and queryselector

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ function getAkanName() {
     event.preventDefault();
     var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+    var myBirthday = document.getElementById("bdate").value;
+    var gender = document.querySelector('input[name = "genders"]:checked').value;
+
 }
