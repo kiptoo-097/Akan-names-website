45ef6cda4ec03d87dcaa8a0f15b89abac787736b
kiptoo-097 <kiptoo097@gmail.c0m>
2020-08-26 15:24:07 +0300

    added loop statement

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,19 @@ function getAkanName() {
     var myBirthday = document.getElementById("bdate").value;
     var gender = document.querySelector('input[name = "genders"]:checked').value;
 
+    var CC = parseInt(myBirthday.substr(0, 2));
+    var YY = parseInt(myBirthday.substr(2, 4));
+    var MM = parseInt(myBirthday.substr(5, 7));
+    var DD = parseInt(myBirthday.substr(8, 9));
+
+    var dateOfBirth = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7);
+
+    if (gender == "Male") {
+        alert("Akan name: " + maleAkanNames[dateOfBirth]);
+    }
+    else if (gender === "Female") {
+        alert("Akan name: " + femaleAkanNames[dateOfBirth]);
+    }
+
+
 }
