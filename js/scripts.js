e6ca33af0b366f774ade2a6341c4afbf5c3a1ff0
kiptoo-097 <kiptoo097@gmail.c0m>
2020-08-31 11:31:36 +0300

    added form validation

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 function getAkanName() {
-    event.preventDefault();
     var femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
     var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
     var myBirthday = document.getElementById("bdate").value;
     var gender = document.querySelector('input[name = "genders"]:checked').value;
+
+    // var btn = gender;
 
     var CC = parseInt(myBirthday.substr(0, 2));
     var YY = parseInt(myBirthday.substr(2, 4));
@@ -13,12 +14,12 @@ function getAkanName() {
 
     var dateOfBirth = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7);
 
-    if (gender == "Male") {
+
+    if (gender === "Male") {
         alert("Akan name: " + maleAkanNames[dateOfBirth]);
     }
     else if (gender === "Female") {
         alert("Akan name: " + femaleAkanNames[dateOfBirth]);
     }
-
 
 }
